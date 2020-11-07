2828e3812397f5c9b31ae6437636ce317e9a9491
tetsu-upstr <bodomu@gmail.com>
2020-11-07 16:58:53 +0900

    add readCsv View

--- CsvHelperApp/CsvHelperApp/Controllers/HomeController.cs
+++ CsvHelperApp/CsvHelperApp/Controllers/HomeController.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CsvHelperApp.Models;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
 
 namespace CsvHelperApp.Controllers
 {
@@ -28,7 +27,21 @@ namespace CsvHelperApp.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult ReadCsv()
+        {
+
+            using (var reader = new StreamReader("/Users/tetsu/user.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<User>();
+            }
+
+            return View();
+        }
+
+
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- CsvHelperApp/CsvHelperApp/Models/User.cs
+++ CsvHelperApp/CsvHelperApp/Models/User.cs
@@ -1,8 +1,13 @@
-﻿namespace CsvHelperApp.Models
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace CsvHelperApp.Models
 {
     public class User
     {
+        [Name("id")]
         public int Id { get; set; }
+
+        [Name("name")]
         public string Name { get; set; }
     }
 }
